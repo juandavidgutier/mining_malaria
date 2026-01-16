443002580ed4d720b6b4260686c2244aba0016b5
juandavidgutier <juandavidgutier@gmail.com>
2026-01-16 12:42:50 -0500

    Update EValue_mining.R

--- EValue_mining.R
+++ EValue_mining.R
@@ -30,22 +30,3 @@ evalue_result <- evalues.RR(
 
 print(evalue_result)
 
-# Interpretation
-cat("\n", rep("=", 80), "\n")
-cat("E-VALUE INTERPRETATION\n")
-cat(rep("=", 80), "\n\n")
-
-cat("The E-value represents the minimum strength of association that an\n")
-cat("unmeasured confounder would need to have (simultaneously with both the exposure\n")
-cat("and the outcome) to fully explain away the observed effect.\n\n")
-
-cat("E-value for the point estimate:", round(evalue_result[1, "E-values"], 2), "\n")
-cat("E-value for the confidence interval limit:", round(evalue_result[2, "E-values"], 2), "\n\n")
-
-if (data$RR_point_estimate > 1) {
-  cat("Since RR > 1, we look for confounders that INCREASE the risk.\n")
-} else if (data$RR_point_estimate < 1) {
-  cat("Since RR < 1, we look for confounders that DECREASE the risk.\n")
-}
-
-cat(rep("=", 80), "\n")
